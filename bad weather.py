983da8b475e6c1fa7ef11585cd15fe5b5513a139
lunar-eclipse26 <sleepysmorz@gmail.com>
2025-08-22 20:48:32 +1200

    Add files via upload

---
+++ bad weather.py
@@ -0,0 +1,6 @@
+def weather_conditions():
+    print("the weather is pretty cold in ",hot_month)
+    print("the weather is also pretty mid in ",medium_month)
+medium_month = "spring"
+hot_month = "summer"
+weather_conditions()
