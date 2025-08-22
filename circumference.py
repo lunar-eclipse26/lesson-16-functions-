d44c8bf1657fb0584580136727ff4c6531cc19ee
lunar-eclipse26 <sleepysmorz@gmail.com>
2025-08-22 21:31:02 +1200

    Add files via upload

---
+++ circumference.py
@@ -0,0 +1,4 @@
+r = float(input("enter the circles radius:"))
+def circumference(r):
+    return 2*3.14*r
+print("the circumference of ur circle is (drum roll pls) this:", circumference(r), "!!!!!!!!!")
